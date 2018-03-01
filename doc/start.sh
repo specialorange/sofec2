2537c1cf051c219aa54cde86e17bb34da08fc808
special <github@specialorange.org>
2018-03-01 14:34:44 -0500

    removing docs

--- doc/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-PATH=$PATH:/home/stefan/.rvm/bin
-source "/home/collin/.rvm/scripts/rvm"
-source ".rvmrc"  # loading local rvmrc if necessary
-# or thin or unicorn start at this point, do NOT start as daemon, upstart handles daemonizing for you
-rails s -e development -p 3000
